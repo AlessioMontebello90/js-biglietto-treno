5c68d6fd0a006621356058a8edfaacb36e721c20
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-19 14:28:40 +0200

    add price generator on JS

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Acquisizione dei dati dall'utente
+let km = parseFloat(
+  prompt("Inserisci il numero di chilometri da percorrere: ")
+);
+let eta = parseInt(prompt("Inserisci la tua età: "));
+
+// Calcolo del prezzo base
+let prezzo_base = km * 0.21;
+
+// Applicazione degli sconti in base all'età
+if (eta < 18) {
+  prezzo_base -= prezzo_base * 0.2; // Sconto del 20% per i minorenni
+} else if (eta >= 65) {
+  prezzo_base -= prezzo_base * 0.4; // Sconto del 40% per gli over 65
+}
+
+// Formattazione del prezzo finale con massimo due decimali
+let prezzo_finale = prezzo_base.toFixed(2);
+
+// Output del prezzo finale
+console.log("Il prezzo finale del tuo viaggio è: " + prezzo_finale + " €");
